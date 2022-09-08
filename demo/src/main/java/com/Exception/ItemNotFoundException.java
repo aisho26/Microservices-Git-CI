26aeebf275a38b1e2ac7559ca3fd7a10be1fde03
aisho26 <s3781589@student.rmit.edu.au>
2022-09-08 23:09:06 +1000

    //created an item class

---
+++ demo/src/main/java/com/Exception/ItemNotFoundException.java
@@ -0,0 +1,8 @@
+package com.Exception;
+
+
+public class ItemNotFoundException extends RuntimeException{
+    public ItemNotFoundException(String exception){
+        super(exception);
+    }
+}
---
+++ demo/src/main/java/com/Model/Item.java
@@ -0,0 +1,57 @@
+package com.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    protected int Id;
+    protected String title;
+    protected String desc;
+    protected String imagePath;
+    protected float price; 
+
+    public int getId(){
+        return this.Id;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getDesc(){
+        return this.desc;
+    }
+
+    public String getImagePath(){
+        return this.imagePath;
+    }
+
+    public float getPrice(){
+        return this.price;
+    }
+
+    public void setId(int Id){
+        this.Id = Id;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setDesc(String desc){
+        this.desc = desc;
+    }
+
+    public void setImagePath(String imagePath){
+        this.imagePath = imagePath;
+    }
+
+    public void setPrice(float price){
+        this.price = price;
+    }
+
+
+    
+}
